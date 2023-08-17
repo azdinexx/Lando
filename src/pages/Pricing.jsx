1dd95942173f1992ac0ced8f7968ac9fe2824b6e
Azz-eddine Bouali <azzdinne.bouali@gmail.com>
2023-08-17 11:30:19 +0100

    pricing cards made selectable

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,4 +1,5 @@
 import GetStarted from '../components/GetStarted';
+import { useState } from 'react';
 
 function Pricing() {
   return (
@@ -19,58 +20,38 @@ function Pricing() {
 export default Pricing;
 
 function Cards() {
+  const [active, setActive] = useState('Basic');
   return (
-    <div className='grid md:grid-cols-3 grid-cols-1 w-[80%]'>
-      <div className='bg-white p-10 rounded-2xl flex flex-col shadow-xl'>
-        <span
-          className='uppercase text-xs mb-5
-        '
-        >
-          Basic
-        </span>
-        <div className='flex gap-1'>
-          <span className='translate-y-2 font-bold'>$</span>
-          <span className='text-6xl font-bold'>9</span>
-          <span className=' self-end'>/month</span>
-        </div>
-        <p className='mt-2  mb-14 text-stone-600'>
-          for individuals and small teams
-        </p>
-        <button className='px-4 py-2 border-black border rounded-xl '>
-          Get Started With Basic
-        </button>
-      </div>
-      <div className='bg-white relative border shadow-xl p-10 rounded-2xl scale-110 flex flex-col'>
-        <span className='bg-blue-500 text-xs text-white absolute top-5 rounded-2xl px-4 py-1 right-5'>
-          Most Popular
-        </span>
-        <span className='uppercase text-xs mb-5'>Pro</span>
-        <div className='flex gap-1'>
-          <span className='translate-y-2 font-bold'>$</span>
-          <span className='text-6xl font-bold'>19</span>
-          <span className=' self-end'>/month</span>
-        </div>
-        <p className='mt-2  mb-14 text-stone-600'>
-          for startups and growing businesses{' '}
-        </p>
-        <button className='px-4 py-2 border-blue-500 bg-blue-500 text-white border rounded-xl '>
-          Get Started With Pro
-        </button>
-      </div>
-      <div className='bg-white p-10 rounded-2xl flex flex-col shadow-xl'>
-        <span className='uppercase text-xs mb-5'>Business</span>
-        <div className='flex gap-1'>
-          <span className='translate-y-2 font-bold'>$</span>
-          <span className='text-6xl font-bold'>99</span>
-          <span className=' self-end'>/month</span>
-        </div>
-        <p className='mt-2  mb-14 text-stone-600'>
-          for Organizations with advanced needs
-        </p>
-        <button className='px-4 text-sm py-2 border-black border rounded-xl '>
-          Get Started With Business
-        </button>
-      </div>
+    <div className='grid md:grid-cols-3 gap-3 grid-cols-1 w-[80%]'>
+      <PricingCard
+        title='Basic'
+        price='9'
+        description='for individuals and small teams'
+        buttonText='Get Started With Basic'
+        isPopular={false}
+        active={active === 'Basic'}
+        setActive={setActive}
+      />
+
+      <PricingCard
+        title='Pro'
+        price='19'
+        description='for startups and growing businesses'
+        buttonText='Get Started With Pro'
+        isPopular={true}
+        active={active === 'Pro'}
+        setActive={setActive}
+      />
+
+      <PricingCard
+        title='Business'
+        price='99'
+        description='for Organizations with advanced needs'
+        buttonText='Get Started With Business'
+        isPopular={false}
+        active={active === 'Business'}
+        setActive={setActive}
+      />
     </div>
   );
 }
@@ -155,5 +136,49 @@ function Checked() {
         d='M20 12a8 8 0 0 1-8 8a8 8 0 0 1-8-8a8 8 0 0 1 8-8c.76 0 1.5.11 2.2.31l1.57-1.57A9.822 9.822 0 0 0 12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10M7.91 10.08L6.5 11.5L11 16L21 6l-1.41-1.42L11 13.17l-3.09-3.09Z'
       />
     </svg>
+  );
+}
+
+function PricingCard({
+  active,
+  title,
+  price,
+  description,
+  isPopular,
+  buttonText,
+  setActive,
+}) {
+  return (
+    <div
+      onClick={() => setActive(title)}
+      className={`${
+        active ? 'scale-110 z-20 opacity-100 border' : ' border-0 opacity-95'
+      } bg-white relative p-10 rounded-2xl flex flex-col shadow-xl cursor-pointer`}
+    >
+      {isPopular && (
+        <span className='bg-blue-500 text-xs text-white absolute top-5 rounded-2xl px-4 py-1 right-5'>
+          Most Popular
+        </span>
+      )}
+      <span
+        className='uppercase text-xs mb-5
+    '
+      >
+        {title}
+      </span>
+      <div className='flex gap-1'>
+        <span className='translate-y-2 font-bold'>$</span>
+        <span className='text-6xl font-bold'>{price}</span>
+        <span className=' self-end'>/month</span>
+      </div>
+      <p className='mt-2  mb-14 text-stone-600'>{description}</p>
+      <button
+        className={`${
+          isPopular ? ' bg-blue-500 text-white border-blue-500' : 'border-black'
+        } px-4 py-2  border rounded-xl `}
+      >
+        {buttonText}
+      </button>
+    </div>
   );
 }
